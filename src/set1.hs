3bee0e35ff5b25d28c2ba89d0b4b48f22538727e
Karl <37496339+aquarial@users.noreply.github.com>
2017-11-16 12:10:19 -0500

    Reorder challenges

--- src/set1.hs
+++ src/set1.hs
@@ -3,11 +3,12 @@ import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Base16 as B16
 
+-- Challenge 1
+hexToBase64 :: ByteString -> ByteString
+hexToBase64 = B64.encode . fst . B16.decode
+
 str :: ByteString
 str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-convert :: ByteString -> ByteString
-convert = B64.encode . fst . B16.decode
 
-ans :: ByteString
-ans = convert str
+-- Challenge 2
