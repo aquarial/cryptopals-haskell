8588c03110055520d37823b10c2e3fcc7149fd06
Karl <37496339+aquarial@users.noreply.github.com>
2017-11-16 12:02:35 -0500

    One try

--- src/set1.hs
+++ src/set1.hs
@@ -1,14 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Word
-import Data.Char
-import Data.Bits
-import Data.List
-import Numeric
-import Data.List.Split (chunksOf)
-import Data.Digits
+import qualified Data.ByteString
+import           Data.Word
+import           Data.List.Split
+import           Numeric
+import           Data.Digits
+import           Data.Char
+
+a = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+num = fst $ head $ readHex a
+
+f xs = intToChar $ fromIntegral $ (\x -> x `mod` 16) $ fst $ head $ readHex xs
 
 d :: [[Char]]
-d = chunksOf 4 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+d = chunksOf 4 a
 
 dd = map (map intToChar . digits 64 . fst) $ concatMap readHex d
 
