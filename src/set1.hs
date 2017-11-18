5c00297ee613ab4e2c27bb43adac739f5a610890
Karl <37496339+aquarial@users.noreply.github.com>
2017-11-18 14:11:59 -0500

    Better xorScore (space is important to english"

--- src/set1.hs
+++ src/set1.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Bits (xor)
-import           Data.Char (ord, isPrint, isAlpha)
+import           Data.Char (ord)
+import qualified Data.Char              as C
 
 import qualified Data.List              as L
 
@@ -13,6 +14,8 @@ import qualified Data.ByteString.Lazy   as BL
 
 import           Control.Lens ((^.))
 import qualified Network.Wreq           as Wreq
+
+import qualified Data.HashMap.Strict    as Map
 
 
 
@@ -45,9 +48,14 @@ c3 = bestXor $ allXors $ fst $ B16.decode "1b37373331363f78151b7f2b783431333d783
 bestXor :: [ByteString] -> ByteString
 bestXor = last . L.sortOn xorScore
 
-xorScore :: ByteString -> Int
-xorScore = B.length . C8.filter isAlpha
-
+xorScore :: ByteString -> Double
+xorScore =  sum . map (\c -> Map.lookupDefault 0 c freq) . map C.toLower . C8.unpack
+  where
+    freq = Map.fromList [('a', 8.167), ('b', 1.492), ('c', 2.782), ('d', 4.253), ('e', 12.70), ('f', 2.228),
+                         ('g', 2.015), ('h', 6.094), ('i', 6.966), ('j', 0.153), ('k', 0.772), ('l', 4.025),
+                         ('m', 2.406), ('n', 6.749), ('o', 7.507), ('p', 1.929), ('q', 0.095), ('r', 5.987),
+                         ('s', 6.327), ('t', 9.056), ('u', 2.758), ('v', 0.978), ('w', 2.360), ('x', 0.150),
+                         ('y', 1.974), ('z', 0.074), (' ', 18.10)]
 
 allXors :: ByteString -> [ByteString]
 allXors bs = [xorB b bs | b <- xorCodes (B.length bs)]
