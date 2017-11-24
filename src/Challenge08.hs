2bb56e6472c2ab2b8fdb0b4b04e99cad1da8a63f
Karl <37496339+aquarial@users.noreply.github.com>
2017-11-23 21:42:11 -0500

    Working on 8.

---
+++ src/Challenge08.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Challenge08 where
+
+import           Challenge03                (englishScore)
+
+import           Data.List
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Lazy.Char8 as C8L
+
+
+import Control.Lens ((^.))
+import qualified Network.Wreq as Wreq
+
+
+c8 :: IO ()
+c8 = do
+  r <- Wreq.get "https://cryptopals.com/static/challenge-data/8.txt"
+  let res = map (fst . B16.decode . C8L.toStrict) $ C8L.split '\n' $ r ^. Wreq.responseBody
+      decrypted = sortOn englishScore $ concatMap tryAllKeys $ sortOn numRepeats res
+  return ()
+
+numRepeats :: ByteString -> Int
+numRepeats = undefined
+numRepeats b = length chnks - length (nub chnks)
+  where
+    chnks = chunks 16 b
+
+chunks :: Int -> ByteString -> [ByteString]
+chunks size bstr | B.length bstr == 0 = []
+                 | otherwise          = B.take size bstr : chunks size (B.drop size bstr)
+
+tryAllKeys :: ByteString -> [ByteString]
+tryAllKeys = undefined
