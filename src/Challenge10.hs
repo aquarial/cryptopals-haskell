8ca8fd4d1507d6d28c1214ea53b937f3f5a3ba56
Karl <37496339+aquarial@users.noreply.github.com>
2017-11-26 21:18:53 -0500

    Finish 10. See readme notes to get ideas for what I found difficult

--- src/Challenge10.hs
+++ src/Challenge10.hs
@@ -2,7 +2,12 @@
 module Challenge10 where
 
 
+import           Challenge02                (xorB)
+import           Challenge06                (decode64, chunks)
+import           Challenge07                (decryptECB)
+
 import           Data.ByteString            (ByteString)
+import qualified Data.ByteString            as B
 import qualified Data.ByteString.Char8      as C8
 import qualified Data.ByteString.Lazy       as BL
 import qualified Data.ByteString.Lazy.Char8 as C8L
@@ -15,8 +20,17 @@ import           Crypto.Cipher.Types        (BlockCipher (ecbDecrypt),
                                              Cipher (cipherInit))
 import           Crypto.Error               (throwCryptoError)
 
-decryptECB :: ByteString -> ByteString -> ByteString
-decryptECB key bytes = ecbDecrypt ctx bytes
-  where
-    ctx :: AES128
-    ctx = throwCryptoError $ cipherInit key
+c10 = do
+  r <- Wreq.get "https://cryptopals.com/static/challenge-data/10.txt"
+  let body = decode64 $ C8L.toStrict $ C8L.filter (/= '\n') $ r ^. Wreq.responseBody
+      parts = decryptCBC "YELLOW SUBMARINE" body
+      text =  parts
+  return text
+
+decryptCBC :: ByteString -> ByteString -> ByteString
+decryptCBC key body = C8.concat $ decryptCBCacc key (B.replicate 16 0) (chunks 16 body)
+
+decryptCBCacc :: ByteString -> ByteString -> [ByteString] -> [ByteString]
+decryptCBCacc key prev []     = []
+decryptCBCacc key prev (m:ms) = xorB prev (decryptECB key m) : decryptCBCacc key m ms
+
