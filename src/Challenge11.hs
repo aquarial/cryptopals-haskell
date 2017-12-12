dc8b78bdb474def2496428d8f444dc9bd6c3b787
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-12 06:33:48 -0500

    Awkward random gen

---
+++ src/Challenge11.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Challenge10 where
+
+import qualified Data.ByteString as B
+import qualified System.Random   as R
+
+key :: R.RandomGen t => t -> (B.ByteString, t)
+key g = let (bytes,g2) = walk 16 [] g in (B.pack bytes, g2)
+  where
+    walk 0 acc g = (reverse acc,g)
+    walk n acc g = let (r,g2) = R.random g in walk (n-1) (r:acc) g2
