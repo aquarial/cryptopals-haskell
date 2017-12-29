4426ed62cb6f83b816afb8fa4a7c57c72aef28e6
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-29 14:56:12 -0600

    Finish generating a random AES key

--- src/Challenge11.hs
+++ src/Challenge11.hs
@@ -1,15 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Challenge10 where
+module Challenge11 where
 
 
 import           Challenge07     (encryptECB)
 import           Challenge10     (decryptCBC)
 
+import           Data.Word
 import qualified Data.ByteString as B
 import qualified System.Random   as R
 
 key :: R.RandomGen t => t -> (B.ByteString, t)
-key g = let (bytes,g2) = walk 16 [] g in (B.pack bytes, g2)
+key g = let (bytes,g2) = walk 16 [] g
+        in (B.pack bytes, g2)
   where
-    walk 0 acc g = (reverse acc,g)
-    walk n acc g = let (r,g2) = R.random g in walk (n-1) (r:acc) g2
+    walk :: R.RandomGen t => Int -> [Word8] -> t -> ([Word8], t)
+    walk 0 acc g = (acc,g)
+    walk n acc g = let (r,g2) = R.random g
+                   in walk (n-1) (r:acc) g2
