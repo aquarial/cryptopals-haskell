f93c4c5b0c0688b994b87299ee716991a7e5b86b
Karl <37496339+aquarial@users.noreply.github.com>
2017-12-29 22:02:14 -0600

    Partway on key and randEncrypt

--- src/Challenge11.hs
+++ src/Challenge11.hs
@@ -2,21 +2,22 @@
 module Challenge11 where
 
 
-import           Challenge07     (encryptECB)
+import           Challenge07     (encryptECB, decryptECB)
 import           Challenge10     (encryptCBC, decryptCBC)
 
 import           Data.Word
 import qualified Data.ByteString as B
 import qualified System.Random   as R
 
-key :: R.RandomGen t => t -> (B.ByteString, t)
-key g = let (bytes,g2) = walk 16 [] g
-        in (B.pack bytes, g2)
+sixteenrandoms :: R.RandomGen g => g -> B.ByteString
+sixteenrandoms g = B.pack . take 16 . R.randoms $ g
+
+randEncrypt g0 text = case choice::Int of
+                        0 -> encryptECB cryptkey text
+                        1 -> encryptCBC cryptkey iv text
   where
-    walk :: R.RandomGen t => Int -> [Word8] -> t -> ([Word8], t)
-    walk 0 acc g = (acc,g)
-    walk n acc g = let (r,g2) = R.random g
-                   in walk (n-1) (r:acc) g2
+    (choice  , g1) = R.randomR (0,1) g0
+    (g2      , g3) = R.split g1
+    cryptkey       = sixteenrandoms g2
+    iv             = sixteenrandoms g3
 
-
-randEncrypt = undefined
