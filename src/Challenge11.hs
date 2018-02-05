c28d35ed2c949c236b372c341ca02b3075337508
Karl <37496339+aquarial@users.noreply.github.com>
2018-02-04 20:58:59 -0500

    Getting somewhere with monad random. Update lts

--- src/Challenge11.hs
+++ src/Challenge11.hs
@@ -5,19 +5,35 @@ module Challenge11 where
 import           Challenge07     (encryptECB, decryptECB)
 import           Challenge10     (encryptCBC, decryptCBC)
 
-import           Data.Word
+import           Data.Monoid ((<>))
+import           Data.Word (Word8)
 import qualified Data.ByteString as B
-import qualified System.Random   as R
+import  Data.ByteString (ByteString)
+import qualified Control.Monad.Random as R
 
-sixteenrandoms :: R.RandomGen g => g -> B.ByteString
-sixteenrandoms g = B.pack . take 16 . R.randoms $ g
 
-randEncrypt g0 text = case choice::Int of
-                        0 -> encryptECB cryptkey text
-                        1 -> encryptCBC cryptkey iv text
+rPad :: R.MonadRandom m => m ByteString
+rPad = do len <- R.getRandomR (5, 10::Int)
+
+          padding <- R.getRandomRs (minBound,maxBound::Word8)
+          pure $ B.pack $ take len padding
+
+padAround :: R.MonadRandom m => ByteString -> m ByteString
+padAround text = do lpad <- R.getRandomR (5,10) >>= randomByteString
+                    rpad <- randomByteString $ 16 + (16 `mod` (B.length lpad + B.length text))
+                    pure $ lpad <> text <> rpad
   where
-    (choice  , g1) = R.randomR (0,1) g0
-    (g2      , g3) = R.split g1
-    cryptkey       = sixteenrandoms g2
-    iv             = sixteenrandoms g3
+roundto n x = x - (x `mod` n)
 
+
+randomByteString :: R.MonadRandom m => Int -> m ByteString
+randomByteString len = B.pack . take len <$> R.getRandomRs (minBound,maxBound)
+
+randEncrypt :: R.MonadRandom m => ByteString -> m ByteString
+randEncrypt itext = do text <- padAround itext
+                       key <- randomByteString 16
+                       mode <- R.getRandomR (False,True)
+                       case mode of
+                         False -> pure $ encryptECB key text
+                         True  -> do iv <- randomByteString 16
+                                     pure $ encryptCBC key iv text
