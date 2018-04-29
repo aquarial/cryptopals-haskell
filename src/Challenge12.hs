ffce0b0fe2c59e2c6eba745a01bc947771d7b9b0
Karl <37496339+aquarial@users.noreply.github.com>
2018-04-29 19:25:26 -0400

    Finish c12 - quite a toughie

--- src/Challenge12.hs
+++ src/Challenge12.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Challenge12 where
 
-import           Challenge07            (encryptECB, decryptECB)
+import           Challenge07            (encryptECB)
+import           Challenge11            (detectEncrypt, EncryptionType(..))
+
 
 import qualified Data.ByteString        as B
 import qualified Data.ByteString.Char8  as C8
@@ -24,27 +26,68 @@ secret = C8.concat
 randomByteString :: R.MonadRandom m => Int -> m B.ByteString
 randomByteString len = B.pack . take len <$> R.getRandoms
 
-randEncrypt :: R.MonadRandom m => B.ByteString -> m B.ByteString
-randEncrypt itext = do
+prefixEncrypt :: R.MonadRandom m => B.ByteString -> m B.ByteString
+prefixEncrypt itext = do
     let Right decoded = B64.decode secret
         txt           = itext <> decoded
         pad           = negate (B.length txt) `mod` 16
     pure $ encryptECB key (txt <> B.replicate pad 0)
 
 
+detectBlockSize :: Monad m => (C8.ByteString -> m C8.ByteString) -> m Int
 detectBlockSize encryptFunc = do
     res <- encryptFunc ""
-    next <-
-        runConduit
-        $ yieldMany [0 ..]
-        .| mapMC (\z -> encryptFunc (C8.replicate z '_'))
-        .| mapC B.length
-        .| filterC (/= B.length res)
-        .| headC
+    next <- runConduit
+              $ yieldMany [0 ..]
+              .| mapMC (\z -> encryptFunc (C8.replicate z '_'))
+              .| mapC B.length
+              .| filterC (/= B.length res)
+              .| headC
     case next of
         Nothing -> error "Could not detect block size"
         Just n  -> pure $ n - B.length res
 
-c12 :: IO Int
-c12 = detectBlockSize randEncrypt
+padToOneLessThan :: C8.ByteString -> Int -> C8.ByteString
+padToOneLessThan b n =
+    case (B.length b + 1) `mod` n of
+      0 -> b
+      _ -> padToOneLessThan (B.cons 0 b) n
 
+
+--  |       |      |
+--  asjfvpoasi
+--  asjf
+--
+--  123456789123456789
+--  0000000aabosidjvi
+--  000000as
+--  00000asj
+--  0000asjf
+--  asdjifaf
+
+
+
+c12 :: IO ()
+c12 = do
+    blocksize <- detectBlockSize prefixEncrypt
+    mode <- detectEncrypt prefixEncrypt
+    msgLen <- B.length <$> prefixEncrypt ""
+    case mode of
+       CBCmode -> putStrLn "Can't break CBC like this yet"
+       ECBmode -> loopBreak "" blocksize msgLen
+  where
+  findOneByte b block prefix = do blk <- prefixEncrypt (B.snoc prefix b)
+                                  if block `B.isPrefixOf` blk
+                                  then pure b
+                                  else findOneByte (b+1) block prefix
+  loopBreak bs size msgLen =
+    if B.length bs >= msgLen
+    then C8.putStrLn bs
+    else do
+      let padding = size - ((B.length bs + 1) `mod` size)
+      matchbytes <- prefixEncrypt $ B.replicate padding 0
+      let nearBlockStart = size * ((padding + B.length bs) `div` size)
+          matchbyte = B.take size $ B.drop nearBlockStart matchbytes
+          prefix = B.drop nearBlockStart (B.replicate padding 0 <> bs)
+      byte <- findOneByte 0 matchbyte prefix
+      loopBreak (B.snoc bs byte) size msgLen
