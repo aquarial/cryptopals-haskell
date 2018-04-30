8756ac488567b4b36bbe94b74c25d9c113a103ee
Karl <37496339+aquarial@users.noreply.github.com>
2018-04-30 13:54:22 -0400

    ECB kv parser running

--- src/Challenge13.hs
+++ src/Challenge13.hs
@@ -1,4 +1,21 @@
 module Challenge13 where
 
+import           Data.Void
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import qualified Data.Map             as M
+
+type Parser = Parsec Void String
+
+parseKV :: Parser (M.Map String String)
+parseKV = foldr ins M.empty <$> oneKV `sepBy` char '&'
+  where
+  ins (k,v) = M.insert k v
+
+oneKV :: Parser (String, String)
+oneKV = do k <- many (letterChar <|> char '@')
+           char '='
+           v <- many letterChar
+           pure (k,v)
 
 c13 = print "not done"
